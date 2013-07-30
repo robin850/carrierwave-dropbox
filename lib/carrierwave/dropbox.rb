3a7165e58a9ffc0e58463bf77f6af98db20556c6
Robin Dupret <robin.dupret@gmail.com>
2013-07-30 12:26:05 +0200

    Initial commit

---
+++ lib/carrierwave/dropbox.rb
@@ -0,0 +1,7 @@
+require "carrierwave/dropbox/version"
+
+module Carrierwave
+  module Dropbox
+    # Your code goes here...
+  end
+end
---
+++ lib/carrierwave/dropbox/version.rb
@@ -0,0 +1,5 @@
+module Carrierwave
+  module Dropbox
+    VERSION = "0.0.1"
+  end
+end
